5b2bed7bdf08f4923687673d6e0d66c7bebfa77a
Alistair Bill <alistair.bill@gmail.com>
2017-09-03 14:24:12 +0100

    Make motor test more comprehensive

--- tests/motors_test.rs
+++ tests/motors_test.rs
@@ -8,9 +8,13 @@ use std::thread;
 #[test]
 fn motor_test() {
     let mut motors = Motors::init();
-    for x in -20..21 {
+    let forwards = (0..21).chain((0..21).rev());
+    let backwards = (-20..1).rev().chain((-20..1));
+
+    for x in forwards.chain(backwards) {
         motors.motors_write(x * 10, x * 10);
         thread::sleep(Duration::from_millis(100));
     }
+
     motors.motors_stop();
 }
