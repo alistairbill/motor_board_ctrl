f9fdd9e1f49e5b6f6081af0067d3a11d89dffe14
Alistair Bill <alistair.bill@gmail.com>
2017-09-04 10:38:51 +0100

    Make motor board test use global state and a mutex

--- tests/motors_test.rs
+++ tests/motors_test.rs
@@ -1,37 +1,42 @@
 extern crate motor_board_ctrl;
 
+#[macro_use]
+extern crate lazy_static;
+
 use motor_board_ctrl::motor_board::MotorBoard;
 
 use std::time::Duration;
 use std::thread;
+use std::sync::Mutex;
+
+lazy_static! {
+    static ref MOTOR_BOARD: Mutex<MotorBoard> = Mutex::new(MotorBoard::init());
+}
 
 #[test]
 #[ignore]
 fn motor_test() {
-    let mut motors = MotorBoard::init();
     let forwards = (0..21).chain((0..21).rev());
     let backwards = (-20..1).rev().chain((-20..1));
 
     for x in forwards.chain(backwards) {
-        motors.motors_write(x * 10, x * 10);
+        MOTOR_BOARD.lock().unwrap().motors_write(x * 10, x * 10);
         thread::sleep(Duration::from_millis(100));
     }
 
-    motors.motors_stop();
+    MOTOR_BOARD.lock().unwrap().motors_stop();
 }
 
 #[test]
 fn trim_test() {
-    let mut motor_board = MotorBoard::init();
-    let trim = motor_board.read_trim();
+    let trim = MOTOR_BOARD.lock().unwrap().read_trim();
     println!("trim = {}", trim);
     assert!(trim >= 0);
 }
 
 #[test]
 fn ir_test() {
-    let mut motor_board = MotorBoard::init();
-    let ir = motor_board.read_ir();
+    let ir = MOTOR_BOARD.lock().unwrap().read_ir();
     for x in ir.iter() {
         println!("ir = {}", x);
         assert!(x >= &0);
